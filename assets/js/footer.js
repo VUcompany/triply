1324ee6d4ee3488a3da16112a91377dc9ddcaefe
VisionUP <79351666+VUcompany@users.noreply.github.com>
2024-12-14 16:54:27 +0900

    asdf

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -5,7 +5,7 @@ var navBar_temp = `
   <div class="row gy-4">
     <div class="col-lg-4 col-md-6 footer-about">
       <a href="index.html" class="logo d-flex align-items-center">
-        <span class="sitename">VisionUP</span>
+        <span class="sitename">VELDER</span>
       </a>
       <div class="footer-contact pt-3">
         
